faddbbbd0e47ae7c2b4732107d5ade6115abbf0a
lino <marcelo.lino.santos@gmail.com>
2022-09-29 18:55:34 -0300

    add new unit test

--- api-account/src/test/java/com/pismo/account/AccountServiceTest.java
+++ api-account/src/test/java/com/pismo/account/AccountServiceTest.java
@@ -5,6 +5,7 @@ import com.pismo.account.repository.AccountRepository;
 import com.pismo.account.service.AccountService;
 import com.pismo.lib.constants.Constants;
 import com.pismo.lib.model.request.AccountRequest;
+import com.pismo.lib.model.request.UpdateAccountRequest;
 import com.pismo.lib.model.response.AccountResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,10 +13,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -62,6 +63,58 @@ public class AccountServiceTest {
         when(repository.findById(any())).thenReturn(accountEntity);
         Exception exception = assertThrows(Exception.class, () -> service.getAccountById(1L));
         assertEquals(String.format(Constants.Messages.ACOOUNT_ID_NOT_FOUND, 1L), exception.getMessage());
+    }
+
+    @Test
+    public void testShouldPutAccountByIdWithSuccess() {
+
+        UpdateAccountRequest updateAccountRequest = UpdateAccountRequest.builder().saldo(new BigDecimal(100)).build();
+        AccountEntity findAccountEntity = AccountEntity
+                .builder()
+                .id(1L)
+                .documentNumber("12345678900")
+                .build();
+
+        AccountEntity saveAccountEntity = AccountEntity
+                .builder()
+                .id(1L)
+                .documentNumber("12345678900")
+                .saldo(new BigDecimal(100))
+                .build();
+
+        when(repository.findById(any())).thenReturn(Optional.ofNullable(findAccountEntity));
+        when(repository.save(any())).thenReturn(saveAccountEntity);
+        AccountResponse accountResponse = service.putAccountId(any(), updateAccountRequest);
+        verify(repository, times(1)).findById(any());
+        verify(repository, times(1)).save(any());
+        assertEquals(accountResponse.getId(), saveAccountEntity.getId());
+        assertEquals(updateAccountRequest.getSaldo(), saveAccountEntity.getSaldo());
+    }
+
+    @Test
+    public void testShouldPutAccountByIdWithFailed() {
+
+        UpdateAccountRequest updateAccountRequest = UpdateAccountRequest.builder().saldo(new BigDecimal(200)).build();
+        AccountEntity findAccountEntity = AccountEntity
+                .builder()
+                .id(1L)
+                .documentNumber("12345678900")
+                .build();
+
+        AccountEntity saveAccountEntity = AccountEntity
+                .builder()
+                .id(1L)
+                .documentNumber("12345678900")
+                .saldo(new BigDecimal(0))
+                .build();
+
+        when(repository.findById(any())).thenReturn(Optional.ofNullable(findAccountEntity));
+        when(repository.save(any())).thenReturn(saveAccountEntity);
+        AccountResponse accountResponse = service.putAccountId(any(),updateAccountRequest);
+        verify(repository, times(1)).findById(any());
+        verify(repository, times(1)).save(any());
+        assertEquals(accountResponse.getId(), saveAccountEntity.getId());
+        assertNotEquals(accountResponse.getSaldo(), saveAccountEntity.getSaldo());
     }
 
 }
